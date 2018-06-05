8c5e7190c9eac89b57e57afe3a3fd45b38dabaf9
ahmad <fstkop@gmail.com>
2018-06-04 20:27:43 -0700

    random prediction added

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print ("hello world")
+import numpy
+
+def color(m1,m2,w1,w2,b):
+  z= m1*w1+m2*w2+b
+  return seg(z)
+
+def seg(y):
+  return 1/(1+numpy.exp(-y))
+
+w1= numpy.random.randn()
+w2= numpy.random.randn()
+b= numpy.random.randn()
+
+print(color(2,2.5,w1,w2,b))

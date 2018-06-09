754678fb880d3a310890e90288b7707e99abf3d8
ahmad <fstkop@gmail.com>
2018-06-08 22:33:40 -0700

    Added ranking and elimination criteria

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-print ("hello world")
+import numpy
+import math
+import string
+import random
+import operator
+
+class gene:
+
+  def __init__(self,num):
+    self.chars= ''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase)for _ in range(num))
+    self.fittness=0;
+
+population=20
+target="Ahmad"
+genes = [gene(5) for _ in xrange(population)]
+
+def ranking():
+  for Gene in genes:
+    for i in range(5):
+      if (Gene.chars[i] == target[i]):
+        Gene.fittness= Gene.fittness+1
+
+def survival():
+   genes.sort(key=operator.attrgetter('fittness'),reverse=True)
+
+
+
+
